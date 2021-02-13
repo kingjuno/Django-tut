36b95170951a2a91ab7442f87086731fc4c2f86b
Geo Jolly <69108486+kingjuno@users.noreply.github.com>
2021-02-13 14:31:53 +0530

    created registeration from

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from register import views as V
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("register/", V.register , name= "register"),
     path('',include("main.urls")),
 ]
