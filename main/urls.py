d31600dc6cba4141019f951c14a9cdcf9aa35297
Geo Jolly <69108486+kingjuno@users.noreply.github.com>
2021-02-13 22:05:48 +0530

    made user specific pages

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,7 @@ urlpatterns =[
     # path("<str:name>",views.index,name="index"),
     # path("v1/",views.v1,name="view 1"),
     path("",views.home,name="home"),
+    path("home/",views.home,name="home"),
     path("create/",views.create,name="create"),
+    path("view/",views.view,name="view"),
 ]
--- main/views.py
+++ main/views.py
@@ -15,25 +15,29 @@ from django.http import HttpResponseRedirect
 
 def index(response,id):
     ls=ToDoList.objects.get(id=id)
-    if response.method=="POST":
-        # print(response.POST)
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                print(response.POST.get("c" + str(item.id)))
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete=True
+    
+    if ls in response.user.todolist.all():
+        
+        if response.method=="POST":
+            # print(response.POST)
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    print(response.POST.get("c" + str(item.id)))
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete=True
+                    else:
+                        item.complete=False  
+                    item.save()
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
+                if len(txt)>2:
+                    ls.item_set.create(text=txt,complete=False)
                 else:
-                    item.complete=False  
-                item.save()
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-            if len(txt)>2:
-                ls.item_set.create(text=txt,complete=False)
-            else:
-                print("Invalid")
-    
-    
-    return render(response,"main/list.html",{"ls":ls})    
+                    print("Invalid")
+        
+        
+        return render(response,"main/list.html",{"ls":ls})
+    return render(response,"main/view.html",{})
 
     
 
@@ -46,9 +50,13 @@ def create(response):
         
         if form.is_valid():
             n = form.cleaned_data["name"]
-            t = ToDoList(name=n)
+            t= ToDoList(name=n)
             t.save()
+            response.user.todolist.add(t)
         return HttpResponseRedirect("/%i"%t.id)
     else:
         form = CreateNewList()
     return render(response,"main/create.html",{"form":form}) 
+
+def view(response):
+    return render(response,"main/view.html",{})
